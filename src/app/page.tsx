3acea84dd623f2a81b5cff984b44e9c2c2c15ff8
Denys Podobrii <denys.podobrii@gmail.com>
2025-08-24 11:24:40 +0300

    fix Image console warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import {AbsoluteCenter, Box, Button, Flex, Heading, VStack} from "@chakra-ui/rea
 import styles from "./page.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
     return (
@@ -9,7 +10,10 @@ export default function Home() {
             <AbsoluteCenter>
                 <VStack>
                     <Heading size='6xl'>Welcome to</Heading>
-                    <Image src='./moviehub_logo.svg' alt='Logo' width={500} height={150} priority={true} className={styles.logo}/>
+                    <Head>
+                        <link rel="preload" href="/moviehub_logo.svg" as="image" />
+                    </Head>
+                    <Image src='/moviehub_logo.svg' alt='Logo' width={500} height={150} priority={true} className={styles.logo}/>
                     <Flex w='100%' justifyContent='center' alignItems='center' gap='4' mt='4'>
                         <Button colorPalette='teal' variant='outline' size='lg' p='4' asChild>
                             <Link href='/login'>
